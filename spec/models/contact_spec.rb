6af19ec49d869cb2223faaaea60c133de4adf073
Kirill Machuhin <km@anahoret.com>
2014-08-12 18:53:48 +0300

    add contact factory

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,46 +2,41 @@ require 'spec_helper'
 
 describe Contact do
   it "is valid with a firstname, lastname and email" do
-    contact = Contact.new(
-      firstname: 'Aaron',
-      lastname: 'Sumner',
-      email: 'tester@example.com')
-    expect(contact).to be_valid
+    expect(build(:contact)).to be_valid
   end
 
   it "is invalid without a firstname" do
-    expect(Contact.new(firstname: nil)).to have(1).errors_on(:firstname)
+    expect(build(:contact, firstname: nil)).to have(1).errors_on(:firstname)
   end
 
   it "is invalid without a lastname" do
-    expect(Contact.new(lastname: nil)).to have(1).errors_on(:lastname)
+    expect(build(:contact, lastname: nil)).to have(1).errors_on(:lastname)
   end
 
   it "is invalid without an email address" do
-    expect(Contact.new(email: nil)).to have(1).error_on(:email)
+    expect(build(:contact, email: nil)).to have(1).error_on(:email)
   end
 
   it "is invalid with a duplicate email address" do
-    Contact.create(email: 'tester@example.com', firstname: 'Aaron', lastname: 'Sumner')
-    contact = Contact.new(email: 'tester@example.com', firstname: 'Aaron', lastname: 'Sumner')
+    create(:contact, email: 'tester@example.com')
+    contact = build(:contact, email: 'tester@example.com')
     expect(contact).to have(1).error_on(:email)
   end
 
   it "returns a contact's full name as a string" do
-    contact = Contact.create(email: 'tester@example.com', firstname: 'Aaron', lastname: 'Sumner')
-    expect(contact.name).to eq("Aaron Sumner")
+    contact = build(:contact)
+    expect(contact.name).to eq("John Doe")
   end
 
   it "returns a sorted array of result that match" do
-    samner = Contact.create(email: 'jhone@example.com', firstname: 'Jhone', lastname: 'Samner')
-    suzuki = Contact.create(email: 'july@example.com', firstname: 'July', lastname: 'Suzuki')
-    huston = Contact.create(email: 'huston@example.com', firstname: 'Huston', lastname: 'Petterson')
+    samner = create(:contact, lastname: 'Samner')
+    suzuki = create(:contact, lastname: 'Suzuki')
 
     expect(Contact.by_letter("S")).to eq [samner, suzuki]
   end
 
   it "is return list according to incoming data" do
-    huston = Contact.create(email: 'huston@example.com', firstname: 'Huston', lastname: 'Petterson')
+    huston = create(:contact, lastname: 'Huston')
 
     expect(Contact.by_letter("J")).to_not include huston
   end
